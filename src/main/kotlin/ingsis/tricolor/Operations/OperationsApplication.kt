922ab1a19ee86a898e9db53210d39dd7a96a3765
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-05-02 16:36:11 -0300

    initial commit

---
+++ src/main/kotlin/ingsis/tricolor/Operations/OperationsApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.tricolor.Operations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OperationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<OperationsApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/tricolor/Operations/OperationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.tricolor.Operations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OperationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
