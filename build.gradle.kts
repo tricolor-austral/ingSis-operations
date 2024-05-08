1353f2283cfb23c5eee15002af6fd777a568e2c3
SantiGrimoldi <Santiago.grimoldi@ing.austral.edu.ar>
2024-05-08 19:24:00 -0300

    added volume for docker and conected the db when running on docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation ("org.postgresql:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
