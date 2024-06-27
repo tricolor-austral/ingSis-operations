5b1feec71e4a634a1be207e650c8ee219b740e20
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-06-27 01:12:27 -0300

    feature: testCaseImprove

--- src/main/kotlin/ingsis/tricolor/operations/dto/testCase/TestCaseReturnDto.kt
+++ src/main/kotlin/ingsis/tricolor/operations/dto/testCase/TestCaseReturnDto.kt
@@ -4,7 +4,6 @@ import ingsis.tricolor.operations.entity.TestCase
 
 class TestCaseReturnDto {
     var id: String = ""
-    var snippetId: Long = 0
     var name: String = ""
     var input: String = ""
     var output: List<String> = mutableListOf()
@@ -13,10 +12,10 @@ class TestCaseReturnDto {
     companion object {
         fun from(testCase: TestCase): TestCaseReturnDto {
             val testCase1 = TestCaseReturnDto()
+            testCase1.id = testCase.id.toString()
             testCase1.input = testCase.input
             testCase1.output = testCase.output
             testCase1.name = testCase.name
-            testCase1.snippetId = testCase.snippetId
             testCase1.envVars = testCase.envVars
             return testCase1
         }
